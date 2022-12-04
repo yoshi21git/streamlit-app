cb3bf0675282ef9c050a4f92379c883a3da1edad
hasegawa <hagm21y@gmail.com>
2022-12-04 22:13:25 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("Web App")
+data = {
+    'lat': np.random.randn(100) / 100 + 35.68,
+    'lon': np.random.randn(100) / 100 + 139.75,
+}
+map_data = pd.DataFrame(data)
+# 地図に散布図を描く
+st.map(map_data)
