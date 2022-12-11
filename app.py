657c39dbfa6f4b5d96b58bf5292a3828349fa3b0
hasegawa <hagm21y@gmail.com>
2022-12-11 20:40:06 +0900

    2nd commit

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.title("Web App")
+st.title("Web Application")
 data = {
-    'lat': np.random.randn(100) / 100 + 35.68,
-    'lon': np.random.randn(100) / 100 + 139.75,
+    'lat': np.random.randn(200) / 100 + 35.68,
+    'lon': np.random.randn(200) / 100 + 139.75,
 }
 map_data = pd.DataFrame(data)
 # 地図に散布図を描く
